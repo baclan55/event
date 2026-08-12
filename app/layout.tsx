e3e289c61b659af63e30d8e2c531208e9bb4e029
Tetsunokishi <barrrerro@gmail.com>
2026-08-12 23:21:51 +0300

    no message

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="ru">
       <head>
-        <link rel="stylesheet" href="/css/site-1.css?v=2" />
-        <link rel="stylesheet" href="/css/site-2.css?v=2" />
-        <link rel="stylesheet" href="/css/site-3.css?v=2" />
-        <link rel="stylesheet" href="/css/site-4.css?v=2" />
+        <link rel="stylesheet" href="/css/site-1.css?v=3" />
+        <link rel="stylesheet" href="/css/site-2.css?v=3" />
+        <link rel="stylesheet" href="/css/site-3.css?v=3" />
+        <link rel="stylesheet" href="/css/site-4.css?v=3" />
       </head>
       <body>
         <script
